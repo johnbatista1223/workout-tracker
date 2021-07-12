f3adf03c6e2239ade5c377e4fb2673124f6ec06f
john batista <batistaj651@gmail.com>
2021-07-12 18:59:39 -0400

    fixing routes

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,13 +1,45 @@
 const router = require("express").Router();
-const path = require("path");
+const Workout = require("../models/workout-module.js");
 
 router.get("/api/workouts", (req, res) => {
-  Transaction.find({})
-
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
+
+router.put("/api/workouts/:id", (req,res) =>{
+  Workout.findOneAndUpdate({id:req.params.id},{$push:{exercises:req.body}})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+      console.log(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+module.exports = router;
